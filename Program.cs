b76341ef1b0e26a7cd44dcfe70e6b18a28b6f768
felipe_l_ferrari <felipe_l_ferrari@estudante.sesisenai.org.br>
2026-06-17 20:03:13 -0300

    questao 11

--- Program.cs
+++ Program.cs
@@ -1,17 +1,6 @@
-﻿ Console.Write("Informe a idade do passageiro: ");
-        int idade = int.Parse(Console.ReadLine());
-
-        Console.Write("Informe o valor normal da passagem: ");
-        double valor = double.Parse(Console.ReadLine());
-
-        double valorFinal;
-
-        if (idade <= 5)
-            valorFinal = 0;
-        else if (idade >= 60)
-            valorFinal = valor / 2;
-        else
-            valorFinal = valor;
-
-        Console.WriteLine("Valor normal da passagem: R$ " + valor);
-        Console.WriteLine("Valor a pagar: R$ " + valorFinal);
+﻿Console.WriteLine("Informe o número final da contagem: ");
+int numeroFinal = int.Parse(Console.ReadLine());
+for (int i = 1; i <= numeroFinal; i++)
+{
+    Console.WriteLine(i);
+}
